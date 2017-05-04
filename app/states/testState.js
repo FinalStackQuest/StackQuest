2392db5a64616d4a814de22fed710d1206fc84b9
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-05-04 15:54:03 -0400

    rename "cameraText" to "backgroundText"

--- app/states/testState.js
+++ app/states/testState.js
@@ -1,7 +1,7 @@
 import StackQuest from '../main'
 
 let cursors
-  , cameraText
+  , backgroundText
   , fixedText
 
 const testState = {
@@ -13,7 +13,7 @@ const testState = {
     //  Modify the world and camera bounds
     StackQuest.world.setBounds(0, 0, 1920, 1080)
 
-    cameraText = StackQuest.add.text(StackQuest.world.width / 2, StackQuest.world.height / 2, 'X', { font: '32px Arial', fill: '#f26c4f', align: 'center' })
+    backgroundText = StackQuest.add.text(StackQuest.world.width / 2, StackQuest.world.height / 2, 'X', { font: '32px Arial', fill: '#f26c4f', align: 'center' })
 
     fixedText = StackQuest.add.text(0, 0, 'O', { font: '32px Arial', fill: '#ffffff', align: 'center' })
     fixedText.fixedToCamera = true
@@ -21,22 +21,22 @@ const testState = {
 
     cursors = StackQuest.input.keyboard.createCursorKeys()
 
-    StackQuest.physics.enable(cameraText, Phaser.Physics.ARCADE)
+    StackQuest.physics.enable(backgroundText, Phaser.Physics.ARCADE)
     StackQuest.physics.enable(fixedText, Phaser.Physics.ARCADE)
   },
 
   update() {
     if (cursors.up.isDown) {
-      if (StackQuest.physics.arcade.collide(cameraText, fixedText)) StackQuest.camera.y += 4
+      if (StackQuest.physics.arcade.collide(backgroundText, fixedText)) StackQuest.camera.y += 4
       else StackQuest.camera.y -= 4
     } else if (cursors.down.isDown) {
-      if (StackQuest.physics.arcade.collide(cameraText, fixedText)) StackQuest.camera.y -= 4
+      if (StackQuest.physics.arcade.collide(backgroundText, fixedText)) StackQuest.camera.y -= 4
       else StackQuest.camera.y += 4
     } if (cursors.left.isDown) {
-      if (StackQuest.physics.arcade.collide(cameraText, fixedText)) StackQuest.camera.x += 4
+      if (StackQuest.physics.arcade.collide(backgroundText, fixedText)) StackQuest.camera.x += 4
       else StackQuest.camera.x -= 4
     } else if (cursors.right.isDown) {
-      if (StackQuest.physics.arcade.collide(cameraText, fixedText)) StackQuest.camera.x -= 4
+      if (StackQuest.physics.arcade.collide(backgroundText, fixedText)) StackQuest.camera.x -= 4
       else StackQuest.camera.x += 4
     }
   },
