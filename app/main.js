3b85c22dc69653f89f57d7f86cdd0a15bd9b1ac8
Anton Cataldi <antoncataldi@gmail.com>
2017-05-04 21:11:36 -0400

    can switch between states!

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ const Phaser = require('phaser')
 
 import testState from './states/testState'
 import mapState from './states/mapState'
+//import anotherMapState from './states/anotherMapState'
 
 // Initialize game
 var StackQuest = new Phaser.Game(1280, 720, Phaser.AUTO, 'main')
@@ -11,6 +12,7 @@ var StackQuest = new Phaser.Game(1280, 720, Phaser.AUTO, 'main')
 // Add all the states
 StackQuest.state.add('testState', testState)
 StackQuest.state.add('mapState', mapState)
+//StackQuest.state.add('anotherMapState', anotherMapState)
 // Start StackQuest Game
 // StackQuest.state.start('testState')
 StackQuest.state.start('mapState')
--- app/states/mapState.js
+++ app/states/mapState.js
@@ -1,6 +1,6 @@
 import StackQuest from '../main'
 
-let cursors, AGuy
+let cursors, AGuy, BGuy
 const mapState = {
   preload: function() {
 
@@ -8,15 +8,21 @@ const mapState = {
   create: function() {
     this.world.setBounds(0, 0, 1920, 1080)
 
+    // Mover
     AGuy = this.add.text(200, 200, 'A', { font: '18px Arial', fill: '#f26c4f', align: 'center' })
     cursors = this.input.keyboard.createCursorKeys()
     this.physics.enable(AGuy, Phaser.Physics.ARCADE)
-    // hi
     AGuy.body.collideWorldBounds = true
+    this.camera.follow(AGuy)
+    // Transportation
+    BGuy = this.add.text(400, 400, 'B', { font: '32px Arial', fill: '#f27c4f', align: 'center' })
+    StackQuest.physics.enable(BGuy, Phaser.Physics.ARCADE)
     console.log('A Guy Stuff', AGuy)
   },
   update: function() {
-    this.physics.arcade.collide(AGuy)
+    if (this.physics.arcade.collide(AGuy, BGuy)) {
+      this.state.start('testState')
+    }
     if (cursors.up.isDown) {
       AGuy.position.y -= 2
     } else if (cursors.down.isDown) {
