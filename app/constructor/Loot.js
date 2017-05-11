d65db8a8e625368a04278305b52640c3e9fa64ab
Anton Cataldi <antoncataldi@gmail.com>
2017-05-11 11:18:55 -0400

    create Loot class skeleton

---
+++ app/constructor/Loot.js
@@ -0,0 +1,7 @@
+import entityPrefab from './entityPrefab'
+
+export default class Loot extends entityPrefab {
+  constructor(game, name, position, spriteKey) {
+    super(game, name, position, spriteKey)
+  }
+}
