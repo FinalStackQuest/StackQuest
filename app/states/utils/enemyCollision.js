39604c0740a244a495b9d3eed5806604db2d4d59
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-05-12 15:52:30 -0400

    add player death text

--- app/states/utils/enemyCollision.js
+++ app/states/utils/enemyCollision.js
@@ -24,6 +24,8 @@ const enemyCollision = (playerObject, projectile, graveyard) => {
         playerObject.position.y = 500
         //  reset internal health: TEMP
         playerObject.internalStats.hp = 100
+        const damage = StackQuest.game.add.text(playerObject.x, playerObject.y, 'YOU DIED', { font: '32px Times New Roman', fill: '#ffa500' })
+        setTimeout(() => damage.destroy(), 1000)
         socket.emit('updatePlayer', playerObject.position)
       }
     })
