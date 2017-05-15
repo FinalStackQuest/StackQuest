733aefb305b204781f2436fe7d4098f78513ba44
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-05-15 13:35:21 -0400

    seperate navbar and footer from root.jsx

---
+++ app/components/Footer.jsx
@@ -0,0 +1,15 @@
+'use strict'
+
+import React from 'react'
+
+const Footer = () => (
+  <footer className="footer footer-container">
+    <div className="container">
+      <div className="vcenter muted">
+        <span className="glyphicon glyphicon-wrench" /> Created by <a href="https://github.com/FinalStackQuest/StackQuest">Team StackQuest</a>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
