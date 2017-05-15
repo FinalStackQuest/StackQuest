0e0dafaa014d3e83b49cb759c63d6ffcad5cc984
Anton Cataldi <antoncataldi@gmail.com>
2017-05-15 17:05:29 -0400

    initial scoreboard on HUD

--- app/classes/HUD.js
+++ app/classes/HUD.js
@@ -31,6 +31,20 @@ class HUD {
     this.HUDElement.numPlayers.setText(`Players in World: ${Object.keys(GamePlayers).length + 1}`)
   }
 
+  updateScoreboard() {
+    // add player names and score to array
+    const playerScores = []
+    for (const player in GamePlayers) {
+      playerScores.push({name: player.name, score: player.lootCount})
+    }
+    playerScores.push({name: this.player.name, score: this.player.lootCount})
+    // sort the array based on lootCount
+    playerScores.sort((p1, p2) => p1.score > p2.score)
+    // add these as text nodes to the
+
+    // this.HUDElement.scoreboard =
+  }
+
   updateFeed(newFeed) {
     this.HUDElement.currentFeed.setText(`${newFeed}`)
     setTimeout(() => this.HUDElement.currentFeed.setText(''), 4000)
@@ -39,6 +53,12 @@ class HUD {
   initHUD() {
     const gameX = this.game.width
     const gameY = this.game.height
+    // const sortedPlayers = GamePlayers.sort()
+    for (const player in GamePlayers) {
+      console.log('name: ', player.name)
+      console.log('lootCount', player.lootCount)
+    }
+    console.log('GamePlayers', GamePlayers)
 
     this.HUDElement.playerName = this.game.add.text(30, 25, `NAME: ${this.player.name}`, {
       font: '15px Press Start 2P',
@@ -65,6 +85,12 @@ class HUD {
     })
 
     this.HUDElement.numPlayers = this.game.add.text(30, 145, `Players in World: ${Object.keys(GamePlayers).length + 1}`, {
+      font: '15px Press Start 2P',
+      fill: '#2a2029',
+      strokeThickness: 1
+    })
+
+    this.HUDElement.scoreboardTitle = this.game.add.text(600, 25, `Top 3 GREEDIEST Players`, {
       font: '15px Press Start 2P',
       fill: '#2a2029',
       strokeThickness: 1
