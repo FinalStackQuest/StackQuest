f289beb50dce638d852dfc2e7d0c4c6ce8834c02
Gabriel <gabriel.wrowe@gmail.com>
2017-05-16 17:14:39 -0400

    feat: add pvp arena

--- app/states/arenaState.js
+++ app/states/arenaState.js
@@ -2,20 +2,15 @@ import { socket, GameGroups } from '../sockets'
 import { createPvpAssets } from './utils/createAssets'
 import makeCollisionMap from './utils/makeCollisionMap'
 import createPlayer from './utils/createPlayer'
-import enemyCollision from './utils/enemyCollision'
 import mapTransition from './utils/mapTransition'
-import itemCollision from './utils/itemCollision'
 import playerCollision from './utils/playerCollision'
 import playerClass from '../classes/Player'
-import Loot from '../classes/Loot'
 
 /* global StackQuest, Phaser */
 
 let map
   , playerObject
   , player
-  , graveyard = []
-  , itemGraveyard = []
 
 const spaceState = {
   init(character) {
@@ -27,8 +22,6 @@ const spaceState = {
 
     map = createPvpAssets()
 
-    GameGroups.items = this.game.add.group()
-    GameGroups.enemies = this.game.add.group()
     GameGroups.players = this.game.add.group()
     GameGroups.HUD = this.game.add.group()
 
@@ -43,25 +36,10 @@ const spaceState = {
     StackQuest.game.physics.arcade.collide(playerObject, StackQuest.game.layers.collisions)
     StackQuest.game.physics.arcade.collide(playerObject, StackQuest.game.layers.collisions_2)
 
-    graveyard.forEach(enemy => {
-      enemy.destroy()
-      socket.emit('killEnemy', enemy.name)
-    })
-    graveyard = []
-
-    itemGraveyard.forEach(item => {
-      item.destroy()
-      socket.emit('killItem', item.name)
-    })
-    itemGraveyard = []
-
     playerObject.movePlayer()
     playerObject.attack()
     playerObject.specialAttack()
     playerObject.HUD.updateNumPlayers()
-
-    itemCollision(playerObject, itemGraveyard)
-    enemyCollision(playerObject, graveyard)
     playerCollision(playerObject)
     mapTransition(player, playerObject, 'fantasyState')
   }
